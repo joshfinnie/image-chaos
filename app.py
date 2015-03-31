ceec4a816a2d4c9c87020f70dff9ca6eb591ca2a
Josh Finnie <josh.finnie@trackmaven.com>
2015-03-31 17:17:09 -0400

    wow, this is terrible...

--- app.py
+++ app.py
@@ -3,7 +3,7 @@
 
 from flask import Flask, render_template
 
-from PIL import Image, ImageFilter
+from PIL import Image, ImageFilter, ImageOps
 import random
 
 app = Flask(__name__)
@@ -11,9 +11,20 @@ app = Flask(__name__)
 FILTER_OPTIONS = [
     ImageFilter.BLUR,
     ImageFilter.CONTOUR,
+    ImageFilter.DETAIL,
     ImageFilter.EDGE_ENHANCE,
+    ImageFilter.EDGE_ENHANCE_MORE,
     ImageFilter.EMBOSS,
+    ImageFilter.FIND_EDGES,
+    ImageFilter.SMOOTH,
+    ImageFilter.SMOOTH_MORE,
     ImageFilter.SHARPEN,
+]
+
+OPS_OPTIONS = [
+    ImageOps.flip,
+    ImageOps.mirror,
+    ImageOps.solarize,
 ]
 
 
@@ -26,12 +37,20 @@ def index():
 def inmage_preview(pic_id):
     i = Image.open('static/images/{}.jpg'.format(pic_id))
     i.save('static/images/{}_processed.jpg'.format(pic_id))
-    for x in range(0, 7):
+    for x in range(3, 10):
         rand_filter = random.randint(0, len(FILTER_OPTIONS)-1)
+        rand_ops = random.randint(0, len(OPS_OPTIONS)-1)
         im = Image.open('static/images/{}_processed.jpg'.format(pic_id))
         im = im.filter(FILTER_OPTIONS[rand_filter])
+        im = OPS_OPTIONS[rand_ops](im)
         im.save('static/images/{}_processed.jpg'.format(pic_id))
     return render_template('display_img.html', image=str(pic_id))
+
+
+@app.after_request
+def add_header(response):
+    response.cache_control.max_age = 0
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
